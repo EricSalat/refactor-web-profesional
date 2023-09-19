db74c9a374a3ebad41f2970ecb42089000de5777
Èric Salat <ericsalat@protonmail.com>
2023-09-19 23:05:41 +0200

    Simplificada la lógica de renderizado del componente Jobs

--- src/app/en/components/Jobs.jsx
+++ src/app/en/components/Jobs.jsx
@@ -8,114 +8,14 @@ import NumberedHeading from '../../components/Numbered-Heading/Numbered-Heading'
 
 export default function Jobs(props) {
 
-  const [digitalResponse, setDigitalResponse] = useState(true)
-  const [arian, setArian] = useState(false);
-  const [poliedric, setPoliedric] = useState(false);
-  const [ainkaa, setAinkaa] = useState(false);
-  const [adiplus, setAdiplus] = useState(false);
-  const [diariAra, setDiariAra] = useState(false);
+  const [activeTab, setActiveTab] = useState("digitalResponse");
 
-  const [digitalResponseTab, setDigitalResponseTab] = useState(true)
-  const [arianTab, setArianTab] = useState(false);
-  const [poliedricTab, setPoliedricTab] = useState(false);
-  const [ainkaaTab, setAinkaaTab] = useState(false);
-  const [adiPlusTab, setAdiPlusTab] = useState(false);
-  const [diariAraTab, setDiariAraTab] = useState(false);
-  
-  return (
-    <>
-      <NumberedHeading texto="Donde he trabajado" />
-      <div className="jobs">
-        <div className="jobs-slider">
-    
-              <TabButton
-              text="Digital Response"
-              class={digitalResponseTab ? "tab-button-active" : "tab-button-inactive"}
-              id="digital"
-              onclick={() => {
-                setArian(false); setPoliedric(false); setAinkaa(false); setAdiplus(false); setDiariAra(false); setDigitalResponse(true);
-                setArianTab(false);
-                setPoliedricTab(false);
-                setAinkaaTab(false);
-                setAdiPlusTab(false);
-                setDiariAraTab(false);
-                setDigitalResponseTab(true);
-              }}
-              />
-
-          <TabButton
-            text="Arian International"
-            id="arian"
-            class={arianTab ? "tab-button-active" : "tab-button-inactive"}
-            onclick={() => {
-              setArian(true); setPoliedric(false); setAinkaa(false); setAdiplus(false); setDiariAra(false); setDigitalResponse(false);
-              setArianTab(true);
-              setPoliedricTab(false);
-              setAinkaaTab(false);
-              setAdiPlusTab(false);
-              setDiariAraTab(false);
-              setDigitalResponseTab(false);
-            }}
-          />
-          <TabButton
-            text="Polièdric"
-            class={poliedricTab ? "tab-button-active" : "tab-button-inactive"}
-            id="poliedric"
-            onclick={() => {
-              setArian(false); setPoliedric(true); setAinkaa(false); setAdiplus(false); setDiariAra(false); setDigitalResponse(false);
-              setArianTab(false);
-              setPoliedricTab(true);
-              setAinkaaTab(false);
-              setAdiPlusTab(false);
-              setDiariAraTab(false);
-              setDigitalResponseTab(false);
-
-            }}
-          />
-          <TabButton text="Ainkaa"
-            class={ainkaaTab ? "tab-button-active" : "tab-button-inactive"}
-            id="ainkaa"
-            onclick={() => {
-              setArian(false); setPoliedric(false); setAinkaa(true); setAdiplus(false); setDiariAra(false); setDigitalResponse(false);
-              setArianTab(false);
-              setPoliedricTab(false);
-              setAinkaaTab(true);
-              setAdiPlusTab(false);
-              setDiariAraTab(false);
-              setDigitalResponseTab(false);
-            }} />
-          <TabButton
-            text="Adiplus Tech & Content"
-            class={adiPlusTab ? "tab-button-active" : "tab-button-inactive"}
-            id="adiplus"
-            onclick={() => {
-              setArian(false); setPoliedric(false); setAinkaa(false); setAdiplus(true); setDiariAra(false); setDigitalResponse(false);
-              setArianTab(false);
-              setPoliedricTab(false);
-              setAinkaaTab(false);
-              setAdiPlusTab(true);
-              setDiariAraTab(false);
-              setDigitalResponseTab(false);
-            }}
-          />
-          <TabButton
-            text="Diari Ara"
-            class={diariAraTab ? "tab-button-active" : "tab-button-inactive"}
-            id="diariara"
-            onclick={() => {
-              setArian(false); setPoliedric(false); setAinkaa(false); setAdiplus(false); setDiariAra(true); setDigitalResponse(false);
-              setArianTab(false);
-              setPoliedricTab(false);
-              setAinkaaTab(false);
-              setAdiPlusTab(false);
-              setDiariAraTab(true);
-              setDigitalResponseTab(false);
-            }}
-          />
-        </div>
-
-        {digitalResponse ? (
-          <JobsCard
+  const tabData = [
+    {
+      id: "digitalResponse",
+      text: "Digital Response",
+      component: (
+        <JobsCard
           id="digital-description"
           role="Frontend and Email Developer"
           href="https://www.digitalresponse.es/"
@@ -125,29 +25,31 @@ export default function Jobs(props) {
           duty2="Developed responsive programming to ensure proper display across various email clients. Conducted testing with Email on Acid."
           duty3="Executed pixel-perfect and detailed designs based on mockups provided in Figma and Adobe XD."
           duty4="Published and monitored Customer Journey campaigns using Adobe Campaigns."
-          />
-          ) : (
-            null
-            )}
-
-        {arian ? (
-          <JobsCard
-            id="arian-description"
-            role="Frontend Developer"
-            period="July - October 2022"
-            company="Arian International Projects"
-            href="https://arianinternational.eu/"
-            duty1="Developed websites using HTML, CSS, and JavaScript, ensuring high-quality code and functionality"
-            duty2="Implemented responsive design techniques to optimize website display across various devices and screen sizes."
-            duty3="Optimized website loading time, improving user experience (UX)and user interface (UI)."
-            duty4="Integrated measurement tools to track and analyze data for digital marketing campaigns."
-            />
-            ) : (
-              null
-              )}
-
-        {poliedric ? (
-          <JobsCard
+        />
+      ),
+    },
+    {
+      id: "arian",
+      text: "Arian International",
+      component: (
+        <JobsCard
+          id="arian-description"
+          role="Frontend Developer"
+          period="July - October 2022"
+          company="Arian International Projects"
+          href="https://arianinternational.eu/"
+          duty1="Developed websites using HTML, CSS, and JavaScript, ensuring high-quality code and functionality"
+          duty2="Implemented responsive design techniques to optimize website display across various devices and screen sizes."
+          duty3="Optimized website loading time, improving user experience (UX)and user interface (UI)."
+          duty4="Integrated measurement tools to track and analyze data for digital marketing campaigns."
+        />
+      ),
+    },
+    {
+      id: "poliedric",
+      text: "Polièdric",
+      component: (
+        <JobsCard
           id="poliedric-description"
           role="Search Engine Marketing (SEM) Specialist"
           company="Polièdric"
@@ -157,29 +59,30 @@ export default function Jobs(props) {
           duty2="Invested +10.000€ monthly in campaigns and increased ROI +300% in different client businesses."
           duty3="Met customers regularly to analyze results and redefine marketing strategies."
           duty4="Audit customers webpages SEO and designed strategies to improve it."
-          />
-          ) : (
-            null
-            )}
-
-
-        {ainkaa ? (
-          <JobsCard
+        />
+      )
+    },
+    {
+      id: "ainkaa",
+      text: "Ainkaa",
+      component: (
+        <JobsCard
           id="ainkaa-description"
           role="Frontend Developer & Project Manager"
-            company="Ainkaa"
-            period="March 2021 - Present"
-            duty1="Start-up creation program for young people to create a viable mobile app. Includes:"
-            duty2="Coded with HTML, CSS, Javascript and React. Designed UI and UX experience. Prototyped with Figma."
-            duty3="Created business and viability plans. Maked marketing plan and benchmarking."
-            duty4="Elevator pitch and MVP presentation for financing search."
-            />
-            ) : (
-              null
-              )}
-
-        {adiplus ? (
-          <JobsCard
+          company="Ainkaa"
+          period="March 2021 - Present"
+          duty1="Start-up creation program for young people to create a viable mobile app. Includes:"
+          duty2="Coded with HTML, CSS, Javascript and React. Designed UI and UX experience. Prototyped with Figma."
+          duty3="Created business and viability plans. Maked marketing plan and benchmarking."
+          duty4="Elevator pitch and MVP presentation for financing search."
+        />
+      )
+    },
+    {
+      id: "adiplus",
+      text: "Adiplus Tech & Content",
+      component: (
+        <JobsCard
           id="adiplus-description"
           role="Journalist"
           href="https://www.linkedin.com/company/adiplus-techandcontent/"
@@ -189,13 +92,14 @@ export default function Jobs(props) {
           duty2="Compose news and reports at Newspa, the magazine of Bakers Guild of the Province of Barcelona."
           duty3="Wrote hot news about technology and insurances at TiempoSeguro, a newspage about the insurance sector dedicated to customers."
           duty4="Designed and implemented SEO strategies and email marketing campaigns."
-          />
-          ) : (
-            null
-            )}
-
-        {diariAra ? (
-          <JobsCard
+        />
+      )
+    },
+    {
+      id: "diariAra",
+      text: "Diari Ara",
+      component: (
+        <JobsCard
           id="diariara-description"
           role="Jorunalist"
           href="https://www.ara.cat/"
@@ -205,14 +109,31 @@ export default function Jobs(props) {
           duty2="Covered hot news."
           duty3="Attended at press conferences."
           duty4="Cooperated evetually with other sections."
-          />
-          ) : (
-            null
-            )}
-
-        
+        />
+      )
+    }
+  ]
+  
+  return (
+    <>
+      <NumberedHeading texto="Donde he trabajado" />
+      <div className="jobs">
+        <div className="jobs-slider">
+          {tabData.map((tab) => (
+            <TabButton
+              key={tab.id}
+              text={tab.text}
+              class={activeTab === tab.id ? "tab-button-active" : "tab-button-inactive"}
+              id={tab.id}
+              onclick={() => setActiveTab(tab.id)}
+            />
+          ))}
+        </div>
+        {tabData.map((tab) =>
+          tab.id === activeTab ? tab.component : null
+        )}
       </div>
-    </>
+  </>    
   );
 }
 
