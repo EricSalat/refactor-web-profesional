f38745f2d2379502760dba53740ec95e09c0fa6e
Èric Salat <ericsalat@protonmail.com>
2023-09-28 20:00:34 +0200

    Arreglado el diseñor responsive del About

--- src/app/page.js
+++ src/app/page.js
@@ -40,29 +40,29 @@ export default function Home() {
         <Hero />
           <section id="about">
             <NumberedHeading texto="Sobre mi" />
-            <div className="about-me">
-              <div className="about-me-text">
-                <p>Hola! Me llamo Èric, vivo en Barcelona y me encanta crear cosas que vivan en Internet. Todo empezó en el año 2020 cuando me uní a un programa para que jóvenes aprendieran a crear aplicaciones móviles y emprendieran sus startups tecnológicas. Allí me metí de lleno a aprender desarrollo web.</p>
-                <p>En la universidad estudié comunicación y marketing, que combinados con mis conocimientos en programación, me permiten ser un perfil híbrido con una visión transversal de los proyectos. Por ello, enfoco el Front-end no solo al desarrollo, sino en la mejora de embudos de tráfico y la conversión de leads y ventas.</p>
-                <p>Estas son las tecnologías con las que he estado trabajando:</p>
-                <div>
-                  <ul className="skills-list">
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>Bootstrap</li>
-                    <li>SASS</li>
-                    <li>JavaScript (ES6+)</li>
-                    <li>React</li>
-                    <li>Next.js</li>
-                    <li>Figma</li>
-                    <li>Adobe XD</li>
-                    <li>Storybook</li>
-                  </ul>
+              <div className="about-me">
+                <div className="about-me-text">
+                  <p>Hola! Me llamo Èric, vivo en Barcelona y me encanta crear cosas que vivan en Internet. Todo empezó en el año 2020 cuando me uní a un programa para que jóvenes aprendieran a crear aplicaciones móviles y emprendieran sus startups tecnológicas. Allí me metí de lleno a aprender desarrollo web.</p>
+                  <p>En la universidad estudié comunicación y marketing, que combinados con mis conocimientos en programación, me permiten ser un perfil híbrido con una visión transversal de los proyectos. Por ello, enfoco el Front-end no solo al desarrollo, sino en la mejora de embudos de tráfico y la conversión de leads y ventas.</p>
+                  <p>Estas son las tecnologías con las que he estado trabajando:</p>
+                  <div>
+                    <ul className="skills-list">
+                      <li>HTML</li>
+                      <li>CSS</li>
+                      <li>Bootstrap</li>
+                      <li>SASS</li>
+                      <li>JavaScript</li>
+                      <li>React</li>
+                      <li>Next.js</li>
+                      <li>Figma</li>
+                      <li>Adobe XD</li>
+                      <li>Storybook</li>
+                    </ul>
+                  </div>
                 </div>
-              </div>
-                <Image
-                 src={FotoEric} height="300" width="300" alt="Retrato de Èric Salat" title="Retrato de Èric Salat" />
-            </div>
+                  <Image
+                   src={FotoEric} height="300" width="300" alt="Retrato de Èric Salat" title="Retrato de Èric Salat" />
+              </div>       
           </section>
           <section id="experience">
             <Trabajos />
